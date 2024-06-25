477ee626953f68708b7ebdf30bc9931e6012e059
David Kuster <dave@talldave.net>
2024-06-25 04:36:56 +0000

    Add idempotency on git clone command

--- scripts/debian/fc-install.sh
+++ scripts/debian/fc-install.sh
@@ -14,7 +14,7 @@ cd ~
 #   firecracker VM lifecycle plugin and API
 # * tc-redirect-tap and other CNI dependencies that enable VMs to start with
 #   access to networks available on the host
-git clone https://github.com/firecracker-microvm/firecracker-containerd.git
+git clone https://github.com/firecracker-microvm/firecracker-containerd.git || true
 cd firecracker-containerd
 sg docker -c 'make all image firecracker'
 sudo make install install-firecracker demo-network
