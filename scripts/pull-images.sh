3558b0ef233b4dee62cdb598ac36655db1f25b22
David Kuster <dave@talldave.net>
2024-06-26 16:59:40 -0500

    Create pull-images.sh

---
+++ scripts/pull-images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker pull deepwonderio/fc-image-builder
+docker tag deepwonderio/fc-image-builder fc-image-builder
+docker pull deepwonderio/runc-builder
+docker tag deepwonderio/fc-image-builder localhost/runc-builder
+docker pull public.ecr.aws/firecracker/fcuvm
