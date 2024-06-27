5282686e4fa9da90d115a21b2f7eea67724570c9
David Kuster <dave@talldave.net>
2024-06-27 18:10:33 -0500

    Update pull-images.sh

--- scripts/pull-images.sh
+++ scripts/pull-images.sh
@@ -4,4 +4,4 @@ docker pull deepwonderio/fc-image-builder
 docker tag deepwonderio/fc-image-builder fc-image-builder
 docker pull deepwonderio/runc-builder
 docker tag deepwonderio/fc-image-builder localhost/runc-builder
-docker pull public.ecr.aws/firecracker/fcuvm
+docker pull public.ecr.aws/firecracker/fcuvm:v35
