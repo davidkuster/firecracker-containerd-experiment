004e449efcbffed194bab9aa8a8390cad134a200
David Kuster <dave@talldave.net>
2024-07-17 21:22:06 +0000

    Create 3.sh

---
+++ firecracker-getting-started/scripts/3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+API_SOCKET="/tmp/firecracker.socket"
+
+# Remove API unix socket
+sudo rm -f $API_SOCKET
+
+# Run firecracker
+sudo ./firecracker --api-sock "${API_SOCKET}"
